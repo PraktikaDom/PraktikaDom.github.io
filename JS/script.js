c550e0849c2de79e9c2544a87cbdcc2808d1e632
PraktikaDom <85159713+PraktikaDom@users.noreply.github.com>
2021-06-02 02:35:09 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,10 @@
+async function getResponse(){
+   	let response = await fetch('https://alexmarchukprod.com.ua/student?id=1')
+   	let content = await response.json()
+   	console.log(content.email)	
+	$('.pupil-text__email').text(content.email)  
+   }
+   //getResponse();
 $(document).ready(function() {
    $(".logo").click(function(){
         window.location.href = '../index.html';
@@ -10,5 +17,16 @@ $(document).ready(function() {
    $(".teacher-link").click(function(){
         window.location.href = 'teachers.html';
     });
+ 	
+   //$.get('https://alexmarchukprod.com.ua/student/?id=1', function(data) {
+   //		data.forEach((item) => {
+   //			const output = `<li">${item.interests}</li>`
+   //			console.log(output);
+   //			$('.pupil-text__interests').appendChild(output);
+   //		});
+   //});
+
+   
+
 
 });	
