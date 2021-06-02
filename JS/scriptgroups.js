15e98d18c933b67a694222f4bcfcc59c64b6d961
PraktikaDom <85159713+PraktikaDom@users.noreply.github.com>
2021-06-03 02:35:16 +0300

    Add files via upload

--- JS/scriptgroups.js
+++ JS/scriptgroups.js
@@ -1,28 +1,34 @@
 
 async function getResponseGroup(){
-   	let response = await fetch(`https://alexmarchukprod.com.ua/groups`);
-   	let content = await response.json();
+    let response = await fetch(`https://alexmarchukprod.com.ua/groups`);
+    let content = await response.json();
      
-   	let key;
-   	let table = document.querySelector('.search__table');
-	for (key in content){
+    let key;
+    let table = document.querySelector('.search__table');
+    
+  if (true) {}
+  for (key in content){
       table.innerHTML+=`<tr class="search__table-tr"></tr>`;
-      let length = content[key].student.length;
-      console.log(length)
-
       if (content[key].formMaster == null) {
-         table.innerHTML+=`
+           table.innerHTML+=`
          <td class="search__table-tdh">${content[key].title}</td>
          <td class="search__table-tdh">П.І.Б.</td>
-         <td class="search__table-tdh">${content[key].title}</td>
-         <td class="search__table-tdh"></td>`
+         <td class="search__table-tdh">${content[key].students.length}</td>
+         <td class="search__table-tdh"><img class="search__table-navig" src="../IMG/expand-more-white-48-dp.svg"></td>`
       }
       else {
          table.innerHTML+=`
          <td class="search__table-tdh">${content[key].title}</td>
          <td class="search__table-tdh">${content[key].formMaster.surname}</td>
-         <td class="search__table-tdh">${content[key].title}</td>
-         <td class="search__table-tdh"></td>`
+         <td class="search__table-tdh">${content[key].students.length}</td>
+         <td class="search__table-tdh"><img class="search__table-navig" src="../IMG/expand-more-white-48-dp.svg"></td>`
       }
    }
 }
+   //$( document ).ready(function(){
+     // $(".search__table-tr").hover(function(){
+      //  $(this).find(".search__table-tdh").css( "background", "#f4f5f6" )
+      //  }, function(){
+       // $(this).find(".search__table-tdh").css( "background", "#ffffff" )
+      //});
+    //});
