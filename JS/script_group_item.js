7118376181689541d54002e7a95e806c9b802db9
PraktikaDom <85159713+PraktikaDom@users.noreply.github.com>
2021-06-03 17:41:59 +0300

    Add files via upload

--- JS/script_group_item.js
+++ JS/script_group_item.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
    $(".logo").click(function(){
         window.location.href = '../index.html';
     }); 
-});	
+
+   $(".pupil-link").click(function(){
+        window.location.href = 'group_item.html';
+    });
+  x = prompt();
+  async function getResponsePupil(user_id){
+      let response = await fetch(`https://alexmarchukprod.com.ua/student?id=${user_id}`);
+      let content = await response.json(); 
+      let list_achivement = document.querySelector('.pupil-text__achivement');
+      $('#group_caption').text(content.birthday)
+  }
+  getResponsePupil(x);
+
+});
