8027e7720694c3e12482e2d6fc4fa8e414f90b04
PraktikaDom <85159713+PraktikaDom@users.noreply.github.com>
2021-06-03 03:03:11 +0300

    Update script_teachers.js

--- JS/script_teachers.js
+++ JS/script_teachers.js
@@ -13,14 +13,14 @@ async function getResponseGroup(){
            table.innerHTML+=`
          <td class="search__table-tdh"><img class="img" src="../IMG/avatar.svg"></td>
          <td class="search__table-tdh">П.І.Б.</td>
-         <td class="search__table-tdh">${content[key].students.length}</td>
+         <td class="search__table-tdh"></td>
          <td class="search__table-tdh"><img class="search__table-navig" src="../IMG/expand-more-white-48-dp.svg"></td>`
       }
       else {
          table.innerHTML+=`
          <td class="search__table-tdh"><img class="" src="../IMG/avatar.svg"></td>
          <td class="search__table-tdh">${content[key].formMaster.surname} ${content[key].formMaster.name} ${content[key].formMaster.patronymic}</td>
-         <td class="search__table-tdh">${content[key].students.length}</td>
+         <td class="search__table-tdh"></td>
          <td class="search__table-tdh"><img class="search__table-navig" src="../IMG/expand-more-white-48-dp.svg"></td>`
       }
    }
