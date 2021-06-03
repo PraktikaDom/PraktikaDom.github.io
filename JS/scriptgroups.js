8a78183d4541079abcf4c43bf3166b02d9e2a9f7
PraktikaDom <85159713+PraktikaDom@users.noreply.github.com>
2021-06-03 13:06:33 +0300

    Add files via upload

--- JS/scriptgroups.js
+++ JS/scriptgroups.js
@@ -4,27 +4,43 @@ async function getResponseGroup(){
     let content = await response.json();
      console.log(content)
     let key;
+    let poop= 5;
     let table = document.querySelector('.search__table');
     
   if (true) {}
   for (key in content){
       table.innerHTML+=`<tr class="search__table-tr"></tr>`;
       if (content[key].formMaster == null) {
-           table.innerHTML+=`
-         <td class="search__table-tdh">${content[key].title}</td>
-         <td class="search__table-tdh">П.І.Б.</td>
-         <td class="search__table-tdh">${content[key].students.length}</td>
-         <td class="search__table-tdh"><img class="search__table-navig" src="../IMG/expand-more-white-48-dp.svg"></td>`
+           table_body.innerHTML+=`
+         <td class="search__table-tdh td_del">${content[key].title}</td>
+         <td class="search__table-tdh td_del">П.І.Б.</td>
+         <td class="search__table-tdh td_del td_oppo">${content[key].students.length}</td>
+         <td class="search__table-tdh td_del"><img class="search__table-navig" src="../IMG/expand-more-white-48-dp.svg"></td>`
       }
       else {
-         table.innerHTML+=`
-         <td class="search__table-tdh">${content[key].title}</td>
-         <td class="search__table-tdh">${content[key].formMaster.surname} ${content[key].formMaster.name} ${content[key].formMaster.patronymic}</td>
-         <td class="search__table-tdh">${content[key].students.length}</td>
-         <td class="search__table-tdh"><img class="search__table-navig" src="../IMG/expand-more-white-48-dp.svg"></td>`
+         table_body.innerHTML+=`
+         <td class="search__table-tdh td_del">${content[key].title}</td>
+         <td class="search__table-tdh td_del">${content[key].formMaster.surname} ${content[key].formMaster.name} ${content[key].formMaster.patronymic}</td>
+         <td class="search__table-tdh td_del td_oppo">${content[key].students.length}</td>
+         <td class="search__table-tdh td_del"><img class="search__table-navig" src="../IMG/expand-more-white-48-dp.svg"></td>`
       }
    }
 }
+getResponseGroup();
+function sort_age() {
+ var tbody =$('#table_body');
+
+ tbody.find('tr').sort(function(a, b) {
+  if($('.search__sorting').val()=='Зростанням') return $(".td_oppo", a).text().localeCompare($('.td_oppo', b).text());
+  else if($('.search__sorting').val()=='Зростанням'){
+    return $(".td").remove();
+    getResponseGroup(); 
+  } 
+  else return $(".td_oppo", b).text().localeCompare($(".td_oppo", a).text());
+}).appendTo(tbody);}
+$(".search__sorting").change(function(){
+  sort_age();
+})
    //$( document ).ready(function(){
      // $(".search__table-tr").hover(function(){
       //  $(this).find(".search__table-tdh").css( "background", "#f4f5f6" )
